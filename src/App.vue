458729e7ef62f9ec9026e54dcfbca533fe496e63
Shan Yu Lin <ineslin35@gmail.com>
2022-02-14 02:23:26 +0800

    feat: add TheSidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
+  <div class="h-screen flex">
+    <TheSidebar />
+    <div class="router-view-container flex">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
-<style>
+<script>
+import { defineComponent } from "vue";
+
+import TheSidebar from "./components/TheSidebar.vue";
+
+export default defineComponent({
+  components: { TheSidebar },
+  setup() {},
+});
+</script>
+
+<style scoped>
+.router-view-container {
+  flex: 0 0 70%;
+  max-width: 70%;
+}
 </style>
---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="sidebar-container p-4 flex flex-col h-full bg-slate-700 text-zinc-50"
+  >
+    <div
+      v-for="(item, index) in menuList"
+      :key="index"
+      class="py-1.5 text-xl border-b border-solid border-sky-50 cursor-pointer"
+    >
+      <router-link :to="item.route"
+        >{{ index + 1 }}. {{ item.name }}</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const menuList = ref([
+      { name: "Bar Chart with Display Animation", route: "/" },
+      {
+        name: "Bar, Line Chart with Standard Deviation Area",
+        route: "/bar-Line-chart-with-standard-deviation-area",
+      },
+    ]);
+
+    return { menuList };
+  },
+});
+</script>
+
+<style scoped>
+.sidebar-container {
+  flex: 0 0 30%;
+  max-width: 30%;
+}
+
+.router-link-active {
+    color: #fcd34d;
+}
+</style>
